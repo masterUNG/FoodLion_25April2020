b5d5b324fee59198068d96475de9bcf44deca99d
masterUNG <phrombutr@gmail.com>
2020-06-11 04:57:53 +0700

    update 11June

--- lib/widget/info_shop.dart
+++ lib/widget/info_shop.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:foodlion/models/user_shop_model.dart';
 import 'package:foodlion/utility/my_api.dart';
 import 'package:foodlion/utility/my_style.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class InfoShop extends StatefulWidget {
@@ -37,7 +38,51 @@ class _InfoShopState extends State<InfoShop> {
       // width: 250.0,
       child: userShopModel == null
           ? MyStyle().showProgress()
-          : Image.network(userShopModel.urlShop),
+          : SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  MyStyle().showTitle(userShopModel.name),
+                  showImage(),
+                  showMap()
+                ],
+              ),
+            ),
+    );
+  }
+
+  Marker yourLocation() {
+    return Marker(
+      markerId: MarkerId('myLoaction'),
+      position: LatLng(
+        double.parse(userShopModel.lat),
+        double.parse(userShopModel.lng),
+      ),infoWindow: InfoWindow(title: userShopModel.name)
+    );
+  }
+
+  Set<Marker> myMarker() {
+    return <Marker>[yourLocation()].toSet();
+  }
+
+  Container showMap() {
+    LatLng latLng = LatLng(
+        double.parse(userShopModel.lat), double.parse(userShopModel.lng));
+    CameraPosition position = CameraPosition(target: latLng, zoom: 16);
+    return Container(
+      width: 250.0,
+      height: 250.0,
+      child: GoogleMap(
+        initialCameraPosition: position,
+        onMapCreated: (controller) {},markers: myMarker(),
+      ),
+    );
+  }
+
+  Container showImage() {
+    return Container(
+      width: 250.0,
+      height: 250.0,
+      child: Image.network(userShopModel.urlShop),
     );
   }
 }
